bb91f26b0a462d5a26deac447af4394afeedfc4b
Giacomo Galloni <giacomo.galloni@unife.it>
2025-08-30 01:08:41 +0200

    test logo in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,8 @@ language = "en"
 html_theme = "sphinx_rtd_theme"  # Use Read the Docs theme
 html_static_path = ["_static"]
 
+html_logo = "_static/logo_cosmoforge_light.png"
+
 # Copy .nojekyll file to output directory for GitHub Pages
 html_extra_path = [".nojekyll"]
 
@@ -103,6 +105,8 @@ html_extra_path = [".nojekyll"]
 html_theme_options = {
     "analytics_id": "",  # Provided by you if you have Google Analytics
     "logo_only": False,
+    "light_logo": "logo_cosmoforge_light.png",
+    "dark_logo": "logo_cosmoforge_dark.png",
     "prev_next_buttons_location": "bottom",
     "style_external_links": False,
     "collapse_navigation": True,
